cd9f11ab81c1e376cb232523b61defdebecf6db4
KwangChi <kwangil.chi@le-campus-numerique.fr>
2020-01-17 15:54:10 +0100

    new .py files added

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b0d1e0cfc3a90e809a904f6eb5a7c06009b8dc8b6230f9d169f38c178073e85
-size 19
+oid sha256:425da1ac99921cb8101fe127e5401a38a5e86f67623aac305f2a624f79e6c725
+size 71
