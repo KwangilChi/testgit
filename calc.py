b46dffdef1c688c61d5684f59d4874707f2b72d5
KwangChi <kwangil.chi@le-campus-numerique.fr>
2020-01-17 16:07:12 +0100

    add files again

---
+++ calc.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:750991b5d79bf23e4a046587f6f10a4cb78dc3cd161617b83ca3d10c5a81464c
+size 10
---
+++ myname.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85d40593ffa13d37e67c255ff4b88cdf70867dd2c407db4dbc389a4a043e1d0a
+size 30
